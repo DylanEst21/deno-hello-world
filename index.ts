5246d721f11d67dc9cc2e4f09acdd57231ab5253
DylanEst21 <dylan.esteves21@gmail.com>
2025-10-15 11:34:00 +0200

    suiteeee

--- index.ts
+++ index.ts
@@ -33,7 +33,7 @@ async function handler(_req: Request): Promise<Response> {
   }
 
   const url = new URL(_req.url);
-  const userWord = (url.searchParams.get("word_guess") ?? "centrale").trim().toLowerCase();
+  const userWord = (url.searchParams.get("wordGuess") ?? "centrale");
 
   const headers = new Headers();
   headers.append("Content-Type", "application/json");
